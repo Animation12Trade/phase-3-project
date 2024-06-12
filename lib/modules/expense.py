390ac83440f3114a1d42f0effb4e601c352980c8
+ <omuhindaaaron@gmail.com>
2024-06-12 08:51:29 +0300

    create expense table

--- lib/modules/expense.py
+++ lib/modules/expense.py
@@ -1,22 +1,22 @@
 from __init__ import conn, cursor
 
-class Income:
-    def __init__(self, source, date, amount, category_id, id=None):
+class Expense:
+    def __init__(self, description, date, amount, category_id, id=None):
         self.id = id
-        self.source = source
+        self.description = description
         self.date = date
         self.amount = amount
         self.category_id = category_id
 
     def __repr__(self):
-        return f"<Income(id={self.id}, source={self.source}, date={self.date}, amount={self.amount}, category_id={self.category_id})>"
+        return f"<Expense(id={self.id}, description={self.description}, date={self.date}, amount={self.amount}, category_id={self.category_id})>"
 
     @classmethod
     def create_table(cls):
         sql = """
-        CREATE TABLE IF NOT EXISTS incomes (
+        CREATE TABLE IF NOT EXISTS expenses (
             id INTEGER PRIMARY KEY,
-            source TEXT NOT NULL,
+            description TEXT NOT NULL,
             date DATE NOT NULL,
             amount FLOAT,
             category_id INTEGER,
@@ -27,24 +27,24 @@ class Income:
         conn.commit()
 
     def save(self):
-        sql = "INSERT INTO incomes (source, date, amount, category_id) VALUES (?, ?, ?, ?)"
-        cursor.execute(sql, (self.source, self.date, self.amount, self.category_id))
+        sql = "INSERT INTO expenses (description, date, amount, category_id) VALUES (?, ?, ?, ?)"
+        cursor.execute(sql, (self.description, self.date, self.amount, self.category_id))
         conn.commit()
         self.id = cursor.lastrowid
 
     @classmethod
     def get_all(cls):
-        sql = "SELECT * FROM incomes"
+        sql = "SELECT * FROM expenses"
         cursor.execute(sql)
         return cursor.fetchall()
 
     @classmethod
     def find_by_id(cls, id):
-        sql = "SELECT * FROM incomes WHERE id = ?"
+        sql = "SELECT * FROM expenses WHERE id = ?"
         cursor.execute(sql, (id,))
         return cursor.fetchone()
 
     def delete(self):
-        sql = "DELETE FROM incomes WHERE id = ?"
+        sql = "DELETE FROM expenses WHERE id = ?"
         cursor.execute(sql, (self.id,))
         conn.commit()
